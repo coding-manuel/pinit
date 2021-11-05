a7deb40a1e27bf1a81cb35558070e35b2b38ceb0
Gautam Manuel <gautam.manuel@gmail.com>
2021-11-05 19:11:11 +0530

    updating git ignore for hosting

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
 	return axios.create({
-		baseURL: "https://pinit-notetaker.herokuapp.com/api",
+		baseURL: "https://pinit-notes.herokuapp.com/api",
 		withCredentials: true,
 		headers: {
 			Accept: "application/json",
